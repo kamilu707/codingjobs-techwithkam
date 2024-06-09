0014a024aae5e0e24e75d2d0ed289ae3557b2fbc
kamal tal <kamaltal123@gmail.com>
2024-06-09 17:33:11 +0200

    Created front page and signup page

--- codingjobs/coreapp/views.py
+++ codingjobs/coreapp/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-\
+
+
+
 def frontpage(request):
-    return render(request, 'coreapp/base.html')
+    return render(request, 'coreapp/frontpage.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+           user = form.save()
+           login(request, user)
+           
+           return redirect('frontpage')
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'coreapp/signup.html', {'form': form})
