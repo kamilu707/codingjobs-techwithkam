bfb42f8aba978bba079aec8e364bf1c57a78e8a9
kamal tal <kamaltal123@gmail.com>
2024-06-17 18:09:50 +0200

    Added job_detail page and readme file

--- codingjobs/codingjobs/urls.py
+++ codingjobs/codingjobs/urls.py
@@ -3,10 +3,15 @@ from django.urls import path
 from coreapp.views import frontpage, signup, logout_view
 from django.contrib.auth import views
 
+from apps.job.views import job_detail
+
+
 urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('signup/', signup, name='signup'),
     path('login/', views.LoginView.as_view(template_name='coreapp/login.html'), name="login"),
-    path('logout/', logout_view, name="logout"),
+    path('logout/', logout_view, name='logout'),
     path('admin/', admin.site.urls),
+    path('jobs/<int:job_id>/', job_detail, name='job_detail'),
+
 ]
